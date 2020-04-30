9949603d87b8d8bed002d5204121bce3e74bca92
LiuAlan1 <alan_liu_is@yahoo.com>
2020-04-30 14:16:10 -0700

    temp

---
+++ Website_Revised_4-29/js/javascript.js
@@ -0,0 +1,194 @@
+var studentNames = [
+['Aaron Urban', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Alejandra Romero', 'B.S. in Visual Communication Design', 'profile_images/Alejandra_Romero_300x300_Final.png', 'https://www.behance.net/aleromero8ebf0'],
+['Alex Bang', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Alex Inokuchi-Garvin', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Alexis Joseph', 'B.S. in Visual Communication Design', 'profile_images/Alexis_Joseph_FINAL.png', 'https://www.behance.net/alexisjose409f'],
+['Alexis De la Torre', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Alyssa Cortez', 'B.S. in Visual Communication Design', 'profile_images/Alyssa_Cortez_FINAL.png', 'https://acortezdesigns.myportfolio.com/portfolio'],
+['Ameer Razman Chin', 'B.S. in Visual Communication Design', 'profile_images/Ameer_RazmanChin_FINAL.png', 'arazmanchin.myportfolio.com'],
+['Amy Lau', 'B.S. in Industrial Design', 'profile_images/Amy_Lau_FINAL.png', 'https://amylau.design/'],
+//['Angela Feng', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Angela Gabriel', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Ashley O\'Brien', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Brett McCormack', 'B.S. in Industrial Design', 'profile_images/Brett_McCormack300x300_Final.png', 'https://portfolio.adobe.com/eea378de-8390-4bfa-883c-cb0d04ccf625/preview/portfolio'],
+['Brian Liu', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Briana Gardener', 'B.S. in Visual Communication Design', 'BrianaGardener_Headshot_final.png', 'www.brianagardener.com'],
+['Calvin Smith', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Carlos Rosas Lopez', 'B.S. in Industrial Design', 'Juan_Rosas_Headshot.jpeg', 'https://www.behance.net/jdrosaslopezdd61'],
+['Casey Goldman', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Charlie Pham', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Chayla King', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Chengming Zhang', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Chi ki Foo', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Chongzhou Wang', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Chris Bateman', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Christian Reyes', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Christopher Nguyen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Christopher Gonzalez', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Cindy Lopez', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Connie Mach', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Connor Dohmen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Cory Vaile', 'B.S. in Visual Communication Design', 'Cory_Vaile_Headshot.png', 'coryvaile.net'],
+['Crisanto Dela Pena', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Crystal Candalla', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Dakota Clark', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Daniel Genis', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Daniel Chu', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Daniela Cabezas', 'B.S. in Visual Communication Design', 'profile_images/daniela_cabezas_FINAL.png', 'portfolio'],
+['Daniela Alcaraz', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['David Matthew Sanchez', 'B.S. in Visual Communication Design', 'profile_images/david_sanchez_FINAL.png', 'https://dav1dsanchez.wixsite.com/davidsanchez'],
+['Dean Cohen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Delmy Cordon', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Devan Ponce', 'B.S. in Industrial Design', 'DevanPonce_Final.png', 'behance.net/devanponce'],
+['Dezza Vidrio', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Dominika Laukova', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Edilson Diaz', 'B.S. in Industrial Design', 'Edilson_Diaz_Headshot.jpg', 'https://ediaz6.myportfolio.com/'],
+['Edward Bond', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Elaina Keegan', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Esther Chin', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Fernando Maldonado', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Francisco Tapia', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Gabriela Pasquini', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Gabrielle Kop', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Hayden Wolf', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Iversander Marroquin', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Jack Tenley', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Jake Smit', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Jason Appler', 'M.A. in Design', 'img', 'portfolio'],
+['Jasper Salomon', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Jaynie Angelique Batin', 'Minor in Industrial Arts', 'img', 'portfolio'],
+['Jenna M. Stamm', 'B.S. in Visual Communication Design', 'JenniferStamm_Headshot_final.png', 'portfolio'],
+['Jesse Vo', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Jessica Noriega', 'M.A. in Design', 'img', 'portfolio'],
+['Jesus Calderon', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Jesus Negrete', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Johnathon Tott-Berger', 'B.S. in Visual Communication Design', 'Johnathon_Tott-Berger_Headshot.jpg', 'https://issuu.com/johnnytotts/docs/tott-berger_j_des_570_final_pages'],
+['Jonathan Ogata', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Jonathan Camacho', 'B.S. in Visual Communication Design', 'JonathanCamacho_Final.png', 'portfolio'],
+['Jonny Ogata', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Juliana Cacciaroni', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Karina Zalac', 'B.S. in Visual Communication Design', 'profile_images/Karina_Zalac_FINAL.png', 'https://karinazalac309539.myportfolio.com/work'],
+['Katherine Berg', 'Minor in Industrial Arts', 'img', 'portfolio'],
+['Kenny Ngo', 'B.S. in Visual Communication Design', 'Kenny_Ngo_headshot_final.png', 'https://behance.net/kennyngo'],
+['Kevin Lambert', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Khang Hoang', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Korina Moreno', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Kristy Chi', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Kwanwu Yu', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Kyle Millhollin', 'Minor in Industrial Arts', 'img', 'portfolio'],
+['Lauren Pattugalan', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Lauren Hohls', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Leilani Paz', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Lemuel Manalo', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Linda Lueng', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Lois Chen', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Madison Macha', 'Minor in Industrial Arts', 'img', 'portfolio'],
+['Mahya Lewis', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Mariana De Araujo Souto', 'M.A. in Design', 'img', 'portfolio'],
+['Mariantonia Cambareri', 'B.S. in Visual Communication Design', 'profile_images/Mariantonia_Cambareri_FINAL.png', 'https://www.behance.net/mariantoniacambareri'],
+['Marielle Cabillo', 'B.S. in Visual Communication Design', 'Marielle_Cabillo_Headshot.jpg', 'instagram.com/mcaboba_final.ai'],
+['Mary Motley', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Mattie Nye', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Mauricio Morazan Mejia', 'B.S. in Visual Communication Design', 'profile_images/Mauricio_Morazan_FINAL.png', 'https://www.bridgegood.com/mauricio/portfolio/9875'],
+['Max Kolden', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Maxwell Lohr', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Mia Dong', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Michael Ihle', 'B.S. in Visual Communication Design', 'profile_images/MichaelIhle_Final.png', 'https://issuu.com/michaelihle/docs/michael_ihle_portfolio'],
+['Michelle Nam', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Monica Chum', 'B.S. in Visual Communication Design', 'profile_images/MonicaChum_final.png', 'portfolio'],
+['Natasha Sherbaf', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Nicolas Noratto', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Nik Dommen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Niklaus Dommen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Noah Castro', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Nora Kettles', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Omar Hernandez', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Pablo Wu', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Pamela Liutama', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Paramveer Singh', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Rachel Ward', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Regine Ong', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Rexford Adams', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Roberto Berrocal-Pujol', 'B.S. in Industrial Design', '(db) Profile Pic.jpg', 'www.disenoberro.com'],
+['Ryan Bach', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Ryan Spalding', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Sabrina Umbay', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Samuel Vasquez', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Sarina Campanella', 'B.S. in Visual Communication Design', 'SarinaCampanella.Final.png', 'Behance.net/SarinaCampanella'],
+['Sean Huang', 'B.S. in Industrial Design', 'profile_images/Sean_Huang_FInal.png', 'portfolio'],
+['Shaine Edmond', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Shea O\'Sullivan', 'B.S. in Visual Communication Design', 'Shea_OSullivan_Headshot.png', 'sheaosullivan.com'],
+['Tanya Hoss', 'B.S. in Visual Communication Design', 'profile_images/TanyaHoss_Final.png', 'portfolio'],
+['Tarrea Tolbert', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Taylor Bosch', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Thomas Kim', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Timothy Green', 'B.A. in Industrial Arts', 'img', 'portfolio'],
+['Tjark Riddle', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Tony Li', 'B.S. in Visual Communication Design', 'profile_images/Tony_Li_Final.png', 'https://www.behance.net/www6575921f3cd'],
+['Travis Marr', 'Minor in Industrial Arts', 'img', 'portfolio'],
+['Tyler Browne', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Victoria Dai', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Vincent Lim', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Vy Nguyen', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Wei Wei Lin', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Wen Liang', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Wilson Lee', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Winnie Manyin Guan', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Winta Semere', 'B.S. in Industrial Design', 'img', 'portfolio'],
+['Yancy Martinez', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Yumiko Inoue', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+['Zhihui Zhang', 'B.S. in Visual Communication Design', 'img', 'portfolio'],
+]
+
+function writeStudentsBatch() {
+    var text = '';
+    for (var i = 0; i <= studentNames.length - 1; i++) {
+        text+= '<div class="grid-item"><a href="student_info.html" onclick="saveStudent(' + i + ')""><img src="';
+        text+= studentNames[i][2]; //image
+        text+= '"></a>\n<h2><a href="student_info.html" onclick="saveStudent(' + i + ')"">';
+        text+= studentNames[i][0]; //name
+        text+= '</a></h2>\n<h3>';
+        text+= studentNames[i][1]; //major
+        text+= '</h3></div>';
+    };
+    return text;
+}
+
+function saveStudent(i) {
+    localStorage.setItem("designatedStudent", i);  
+}
+
+/*
+THE BELOW FUNCTIONS ARE FOR THE STUDENT INFO PAGE
+*/
+
+var designatedStudent = parseInt(localStorage.getItem('designatedStudent'));
+
+function getStudentInfoName() {
+    return studentNames[designatedStudent][0];
+}
+
+function getStudentInfoMajor() {
+    return studentNames[designatedStudent][1];
+}
+
+function getStudentInfoImg() {
+    text = '<img src="' + studentNames[designatedStudent][2] + '">';
+    return text;
+}
+
+function getStudentInfoPortfolio() {
+    text = '<a href="' + studentNames[designatedStudent][3] + '">Portfolio</a>';
+    return text;
+}
+
+function printStudentSidebar() {
+    text = '<a href="index.html" style="font-size: 130%">Index</a>'
+    for (var i = 0; i <= studentNames.length - 1; i++) {
+        text += '<hr><a href="student_info.html" onclick="saveStudent(' + i + ')"">';
+        text += studentNames[i][0]; //name
+        text += '</a>';
+    };
+    return text;
+}
